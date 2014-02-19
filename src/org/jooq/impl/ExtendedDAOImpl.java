03d417fb574b9d278a4eec0976a7d5028ec79ffa
Aurélien Manteaux <amanteaux@cardiweb.com>
2014-02-19 14:06:08 +0100

    The extended DAO : - does not inherit DAOImpl anymore - isNew and save methods deleted

--- src/org/jooq/impl/ExtendedDAOImpl.java
+++ src/org/jooq/impl/ExtendedDAOImpl.java
@@ -1,29 +1,152 @@
 package org.jooq.impl;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.jooq.Configuration;
 import org.jooq.ConfigurationExtended;
 import org.jooq.DAOExtended;
+import org.jooq.Field;
+import org.jooq.RecordMapper;
 import org.jooq.Table;
 import org.jooq.UpdatableRecord;
 
-public abstract class ExtendedDAOImpl<R extends UpdatableRecord<R>, P, T> extends DAOImpl<R, P, T> implements DAOExtended<R, P, T> {
+public abstract class ExtendedDAOImpl<R extends UpdatableRecord<R>, P, T> implements DAOExtended<R, P, T> {
+	
+	private final DAOImpl<R, P, T> delegate;
+	private final ConfigurationExtended configurationExtended;
 
 	public ExtendedDAOImpl(final Table<R> table, final Class<P> type) {
-		super(table, type);
+		this(table, type, null);
 	}
 
-	protected ExtendedDAOImpl(final Table<R> table, final Class<P> type, final ConfigurationExtended configuration) {
-		super(table, type, configuration);
+	protected ExtendedDAOImpl(final Table<R> table, final Class<P> type, final ConfigurationExtended configurationExtended) {
+		this.delegate = new DAOImpl<R, P, T>(table, type, configurationExtended) {
+
+			@Override
+			protected T getId(P object) {
+				return ExtendedDAOImpl.this.getId(object);
+			}
+			
+		};
+		this.configurationExtended = configurationExtended;
 	}
 	
 	@Override
 	public ConfigurationExtended configurationExtended() {
-		return (ConfigurationExtended) configuration();
+		return configurationExtended;
+	}
+	
+    // ------------------------------------------------------------------------
+    // XXX: DAOImpl override
+    // ------------------------------------------------------------------------
+
+	public final void insert(Collection<P> objects) {
+		for(P object : objects) {
+			setId(object, configurationExtended.idGenerator().generate(Long.class));
+		}
+		delegate.insert(objects);
+	}
+	
+    // ------------------------------------------------------------------------
+    // XXX: Delegate calls
+    // ------------------------------------------------------------------------
+	
+	public final void setConfiguration(Configuration configuration) {
+		delegate.setConfiguration(configuration);
 	}
 
-	protected abstract void setId(P object, java.lang.Long id);
+	public final Configuration configuration() {
+		return delegate.configuration();
+	}
 
-	public abstract boolean isNew(P object);
+	public RecordMapper<R, P> mapper() {
+		return delegate.mapper();
+	}
 
-	public abstract void save(P object);
+	public final void insert(P object) {
+		delegate.insert(object);
+	}
 
+	public final void insert(P... objects) {
+		delegate.insert(objects);
+	}
+
+	public final void update(P object) {
+		delegate.update(object);
+	}
+
+	public final void update(P... objects) {
+		delegate.update(objects);
+	}
+
+	public final void update(Collection<P> objects) {
+		delegate.update(objects);
+	}
+
+	public final void delete(P... objects) {
+		delegate.delete(objects);
+	}
+
+	public final void delete(Collection<P> objects) {
+		delegate.delete(objects);
+	}
+
+	public final void deleteById(T... ids) {
+		delegate.deleteById(ids);
+	}
+
+	public final void deleteById(Collection<T> ids) {
+		delegate.deleteById(ids);
+	}
+
+	public final boolean exists(P object) {
+		return delegate.exists(object);
+	}
+
+	public final boolean existsById(T id) {
+		return delegate.existsById(id);
+	}
+
+	public final long count() {
+		return delegate.count();
+	}
+
+	public final List<P> findAll() {
+		return delegate.findAll();
+	}
+
+	public final P findById(T id) {
+		return delegate.findById(id);
+	}
+
+	public final <Z> List<P> fetch(Field<Z> field, Z... values) {
+		return delegate.fetch(field, values);
+	}
+
+	public final <Z> P fetchOne(Field<Z> field, Z value) {
+		return delegate.fetchOne(field, value);
+	}
+
+	public final Table<R> getTable() {
+		return delegate.getTable();
+	}
+
+	public final Class<P> getType() {
+		return delegate.getType();
+	}
+	
+    // ------------------------------------------------------------------------
+    // XXX: Template methods for generated subclasses
+    // ------------------------------------------------------------------------
+	
+	protected abstract T getId(P object);
+	
+	/**
+	 * Set a primary key value to the POJO
+	 * @param object
+	 * @param id
+	 */
+	protected abstract void setId(P object, Long id);
+	
 }
